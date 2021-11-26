1392e331922138008a1427bed9bd9ab24d484db7
Lalá Nametala <lalanametala@gmail.com>
2021-11-26 19:24:05 -0300

    exercicio 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-1.js
@@ -1,8 +1,8 @@
-const a=-1;
-const b=3;
-const c=5;
+const a=0;
+const b=2;
+const c=8;
 
-if (a%2==0 || b%2==0 || c%2==0){
+if (a%2!==0 || b%2!==0 || c%2!==0){
     console.log(true)
 } else {
     console.log (false);    
