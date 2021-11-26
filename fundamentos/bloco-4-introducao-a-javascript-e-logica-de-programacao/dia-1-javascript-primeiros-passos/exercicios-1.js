76df10ea54cb8ad5b3b53e985dbfddb4e103dc89
Lalá Nametala <lalanametala@gmail.com>
2021-11-26 19:11:20 -0300

    exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-1.js
@@ -1,11 +1,10 @@
 const a=1;
-const b=2;
-const c=7;
 
-if (a>b && a>c) {
-    console.log (a);
-} else if (b>a && b>c) {
-    console.log(b);
+
+if (a>0) {
+    console.log ("positive");
+} else if (a<0) {
+    console.log("negative");
 } else {
-    console.log(c);
+    console.log("zero");
 }
