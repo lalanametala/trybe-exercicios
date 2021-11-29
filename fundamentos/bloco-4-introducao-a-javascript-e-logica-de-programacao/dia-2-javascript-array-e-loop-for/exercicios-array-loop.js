cd9825f895a72074f0ed374fd20e387a76253c5c
Lalá Nametala <lalanametala@gmail.com>
2021-11-29 18:31:58 -0300

    exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-array-loop.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-array-loop.js
@@ -1,13 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numerosImpares=0;
+let menorNumero=numbers[0];
 
 for (let number of numbers){
-    if (number%2!=0){
-        numerosImpares+=1;
+    if (number<menorNumero){
+        menorNumero=number;
     }
 }
-if (numerosImpares==0){
-    console.log("nenhum valor ímpar encontrado");
-} else {
-    console.log(numerosImpares);
-}
+console.log(menorNumero);
