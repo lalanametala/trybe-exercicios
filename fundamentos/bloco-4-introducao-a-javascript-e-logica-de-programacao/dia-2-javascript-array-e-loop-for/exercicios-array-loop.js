86873ebe286d55a71cbf90fb698d1cfac506bf97
Lalá Nametala <lalanametala@gmail.com>
2021-11-29 18:28:01 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-array-loop.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-array-loop.js
@@ -1,13 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma=0;
+let maiorNumero=0;
 
 for (let number of numbers){
-    soma+=number;
+    if (number>maiorNumero){
+        maiorNumero=number;
+    }
 }
-let media=soma/(numbers.length);
-console.log(media);
-if (media>20){
-    console.log("valor maior que 20");
-} else {
-    console.log("valor menor ou igual a 20");
-}
+console.log(maiorNumero);
