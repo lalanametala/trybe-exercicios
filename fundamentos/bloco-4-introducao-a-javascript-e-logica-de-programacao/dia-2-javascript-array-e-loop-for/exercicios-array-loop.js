6531f7bf6913e1d0bcee5328f3aa1c031ed9af99
Lalá Nametala <lalanametala@gmail.com>
2021-11-29 18:34:56 -0300

    exercicio 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-array-loop.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-array-loop.js
@@ -1,9 +1,6 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorNumero=numbers[0];
+let numbers = [];
 
-for (let number of numbers){
-    if (number<menorNumero){
-        menorNumero=number;
-    }
+for (let i=1;i<=25;i+=1){
+    numbers.push(i);
 }
-console.log(menorNumero);
+console.log(numbers);
