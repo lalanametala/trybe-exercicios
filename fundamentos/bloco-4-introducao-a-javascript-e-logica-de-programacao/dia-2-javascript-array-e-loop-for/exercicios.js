7ee49d12bc9771d0ad6b79801b27aa52e7a1b2d7
Lalá Nametala <lalanametala@gmail.com>
2021-11-29 15:45:05 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,4 +1,4 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu.indexOf('Serviços');
+let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(menuServices);
+console.log(indexOfPortfolio);
