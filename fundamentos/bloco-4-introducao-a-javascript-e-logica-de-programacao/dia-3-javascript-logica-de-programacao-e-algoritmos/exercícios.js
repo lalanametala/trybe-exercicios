67e74183830636984baa6e72e822c15a097f7ca8
Lalá Nametala <lalanametala@gmail.com>
2021-11-30 18:37:24 -0300

    exercicio 4 - nao entendi muito bem

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercícios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercícios.js
@@ -1,14 +1,20 @@
 function invertedTriangle (n) {
-    for (let i=0;i<n;i++){
+    let centro=(n+1)/2;
+    let espacoDireita=centro;
+    let espacoEsquerda=centro;
+    
+    for (let i=0;i<=centro;i++){
         let linha="";
-        for (let j=0; j<n; j++){
-            if (j<(n-i-1)){
-                linha+=" ";
-            } else {
+        for (let j=0; j<=n; j++){
+            if (j>espacoEsquerda && j<espacoDireita){
                 linha+="*";
+            } else {
+                linha+=" ";
             }
         }  
         console.log(linha);
+        espacoDireita+=1;
+        espacoEsquerda-=1;
     }
     return;
 }
