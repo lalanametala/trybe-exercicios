6c1c465d774721ca517772850c9601c6399b4ca0
Lalá Nametala <lalanametala@gmail.com>
2021-11-30 19:05:42 -0300

    exercicio 4 - entendido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercícios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercícios.js
@@ -1,10 +1,11 @@
-function invertedTriangle (n) {
+function pyramid (n) {
     let centro=(n+1)/2;
     let espacoDireita=centro;
     let espacoEsquerda=centro;
-    
+    let linha="";
+
     for (let i=0;i<=centro;i++){
-        let linha="";
+        
         for (let j=0; j<=n; j++){
             if (j>espacoEsquerda && j<espacoDireita){
                 linha+="*";
@@ -13,10 +14,11 @@ function invertedTriangle (n) {
             }
         }  
         console.log(linha);
+        linha="";
         espacoDireita+=1;
         espacoEsquerda-=1;
     }
     return;
 }
 
-invertedTriangle(5);
+pyramid(5);
