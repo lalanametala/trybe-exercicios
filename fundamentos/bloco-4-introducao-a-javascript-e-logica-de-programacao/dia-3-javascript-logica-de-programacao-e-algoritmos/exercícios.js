1f8c1b5cdbb149d2aa33b95cc072869394fe705f
Lalá Nametala <lalanametala@gmail.com>
2021-11-30 19:41:53 -0300

    exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercícios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercícios.js
@@ -1,35 +1,18 @@
-// Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
-// n = 7
 
-//   *
-//  * *
-// *   *
-//*******
+function ehPrimo (n) {
+    let divisores=0;
 
-function emptyPyramid (n) {
-    let centro=(n+1)/2;
-    let espacoDireita=centro;
-    let espacoEsquerda=centro;
-    let linha="";
-
-    for (let i=0;i<=centro;i++){
-        
-        for (let j=0; j<=n; j++){
-            if (i==centro) {
-                linha=" "+"*".repeat(n);
-            } else if (j==espacoEsquerda || j==espacoDireita) {
-                linha+="*";
-            }
-            else {           
-                linha+=" ";
-            }        
-        }  
-        console.log(linha);
-        linha="";
-        espacoDireita+=1;
-        espacoEsquerda-=1;
+    for (let i=1;i<=n;i++){
+        if (n%i==0){
+            divisores+=1;
+        }
+    }
+    if (divisores>2){
+        console.log("Esse número não é primo");
+    } else {
+        console.log("Esse número é primo");
     }
     return;
 }
 
-emptyPyramid(7);
+ehPrimo(8);
