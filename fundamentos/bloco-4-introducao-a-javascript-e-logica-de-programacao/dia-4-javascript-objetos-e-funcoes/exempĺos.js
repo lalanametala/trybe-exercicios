b15a6d4542721f6a8f12ad48814509712050a660
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 15:20:20 -0300

    exercicio de fixaçao 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exempĺos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exempĺos.js
@@ -6,7 +6,8 @@ let player = {
     medals : {
         golden: 2, 
         silver: 3,
-    }
+    },
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
 }
 
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.")
