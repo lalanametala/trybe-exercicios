71195b52d8c65100873fc76ac8aff746701c6cf7
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 20:16:02 -0300

    BONUS - exercicio 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,19 +1,52 @@
-//Parte II - Funções
-let palavra="";
-function finalString(word,ending){
-    let teste=false;
-    let drow = word.split('').reverse();
-    let gnidne = ending.split('').reverse();
+//Bônus
 
-    for (let position in gnidne){        
-        if (gnidne[position]==drow[position]){
-            teste=true;
-        } else {
-            teste=false;
+function singleRomanToArabic(numero) {
+    switch (numero){
+        case "I":
+            numero=1;
             break;
-        }        
+        case "V":
+            numero=5;
+            break;
+        case "X":
+            numero=10;
+            break;
+        case "L":
+            numero=50;
+            break;
+        case "C":
+            numero=100;
+            break;
+        case "D":
+            numero=500;
+            break;
+        case "M":
+            numero=1000;
+            break;        
     }
-
-    return teste;
+    return numero;
 }
-console.log(finalString('joaofernando', 'fernan'));
+
+function romanToArabic(romanNumber){
+    let numArabico=0;
+    let numAnterior=0;
+    let numAtual=0;
+    for (let caractere in romanNumber){
+        numAtual=singleRomanToArabic(romanNumber[caractere]);
+        if (romanNumber.length==1){
+            numArabico=numAtual;
+        } else if (caractere==0) {
+            numAnterior=numAtual;
+            numArabico=numAtual;                  
+        } else if (numAtual>numAnterior && romanNumber.length==2){
+            numArabico=numAtual-numAnterior;
+            break;
+        } else {
+            numArabico+=numAtual;
+            numAnterior=numAtual;
+        }
+    }
+    return numArabico;
+}
+
+console.log(romanToArabic("MIII"));
