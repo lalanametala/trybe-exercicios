dbe32fa033515e97434de8c9aa56eccb1b5deab9
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 15:33:00 -0300

    exercicio de fixaçao for/of 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exempĺos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exempĺos.js
@@ -1,13 +1,9 @@
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: 34,
-    medals : {
-        golden: 2, 
-        silver: 3,
-    },
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
-}
-
-console.log("A jogadora possui " + player.medals.golden + " medalhas de ouro e " + player.medals.silver + " medalhas de prata.")
+  for (let nome in names) {
+      console.log("Olá, "+names[nome]);
+  }
