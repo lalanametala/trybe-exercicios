28885ebbab22bd7f6bcc738546b574bc084bbdeb
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 20:20:12 -0300

    BONUS - exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,52 +1,15 @@
 //Bônus
 
-function singleRomanToArabic(numero) {
-    switch (numero){
-        case "I":
-            numero=1;
-            break;
-        case "V":
-            numero=5;
-            break;
-        case "X":
-            numero=10;
-            break;
-        case "L":
-            numero=50;
-            break;
-        case "C":
-            numero=100;
-            break;
-        case "D":
-            numero=500;
-            break;
-        case "M":
-            numero=1000;
-            break;        
-    }
-    return numero;
-}
-
-function romanToArabic(romanNumber){
-    let numArabico=0;
-    let numAnterior=0;
-    let numAtual=0;
-    for (let caractere in romanNumber){
-        numAtual=singleRomanToArabic(romanNumber[caractere]);
-        if (romanNumber.length==1){
-            numArabico=numAtual;
-        } else if (caractere==0) {
-            numAnterior=numAtual;
-            numArabico=numAtual;                  
-        } else if (numAtual>numAnterior && romanNumber.length==2){
-            numArabico=numAtual-numAnterior;
-            break;
-        } else {
-            numArabico+=numAtual;
-            numAnterior=numAtual;
+function vectorUnifier(vetor){
+    let resposta = [];
+    for (let array of vetor){
+        for (let num of array){
+            resposta.push (num);
         }
     }
-    return numArabico;
+    return resposta;
 }
 
-console.log(romanToArabic("MIII"));
+
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+console.log(vectorUnifier(vector));
