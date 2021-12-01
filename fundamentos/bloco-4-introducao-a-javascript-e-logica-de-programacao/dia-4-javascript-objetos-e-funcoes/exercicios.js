569b0ddead253905fb2e2a78b2cf7f5c1f2c7b95
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 19:32:28 -0300

    parte 2 - exercicio 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,12 +1,19 @@
 //Parte II - Funções
 let palavra="";
-function somatorio(numero){
-    let soma=0;
-    
-    for (let index=1;index<=numero;index+=1){
-        soma+=index;
+function finalString(word,ending){
+    let teste=false;
+    let drow = word.split('').reverse();
+    let gnidne = ending.split('').reverse();
+
+    for (let position in gnidne){        
+        if (gnidne[position]==drow[position]){
+            teste=true;
+        } else {
+            teste=false;
+            break;
+        }        
     }
 
-    return soma;
+    return teste;
 }
-console.log(somatorio(5));
+console.log(finalString('joaofernando', 'fernan'));
