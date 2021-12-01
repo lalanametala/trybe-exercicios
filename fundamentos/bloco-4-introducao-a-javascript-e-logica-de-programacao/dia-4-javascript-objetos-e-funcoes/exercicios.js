6b7ac1078d3b347a9b5c5a6f5c4ee55c62b61d8a
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 18:35:56 -0300

    parte 2 - exercicio 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,20 +1,16 @@
-let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
+//Parte II - Funções
+let palavra="";
+function verificaPalindromo(palavra){
+    let arranjoPalavra=palavra.split("");    
+    let arranjoPalavraReversa=arranjoPalavra.reverse();
+    let palavraReversa=arranjoPalavraReversa.join('');
+    let retorno;
+    if (palavra===palavraReversa){
+        retorno=true;
+    } else {
+        retorno=false;
+    }
+    return retorno;
+}
 
-  leitor.livrosFavoritos.push({
-    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editor: 'Rocco',
-  });
-
-  console.log(leitor.nome+" tem "+leitor.livrosFavoritos.length+" livros favoritos");
+console.log(verificaPalindromo("desenvolvimento"));
