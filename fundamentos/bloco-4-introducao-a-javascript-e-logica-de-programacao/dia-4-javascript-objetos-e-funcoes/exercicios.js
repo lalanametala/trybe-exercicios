0ac48df55573a0b512fafff9ed805e0bb1c86016
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 17:56:57 -0300

    exercicio 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -4,4 +4,6 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log("Bem-vinda, "+info.personagem);
+  info.recorrente='Sim';
+
+  console.log(info);
