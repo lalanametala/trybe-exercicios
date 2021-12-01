a6b9bc9cba93a2ff622fc932f6c5366025792332
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 19:20:23 -0300

    parte 2 - exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,23 +1,12 @@
 //Parte II - Funções
 let palavra="";
-function maisRepeticoes(arranjo){
-    let maisRepetidoAtual=0;
-    let numeroRepeticoesMaior=0;
-    for (let position in arranjo){
-        let contadorRepeticoes=0;
-        for (let posicao in arranjo){  
-            if (posicao!=position){
-                if (arranjo[position]==arranjo[posicao]){
-                    contadorRepeticoes+=1;
-                }
-            }
-        }
-        if (contadorRepeticoes>numeroRepeticoesMaior){
-            numeroRepeticoesMaior=contadorRepeticoes;
-            maisRepetidoAtual=arranjo[position];
-        }
+function somatorio(numero){
+    let soma=0;
+    
+    for (let index=1;index<=numero;index+=1){
+        soma+=index;
     }
-    return maisRepetidoAtual;
+
+    return soma;
 }
-let teste =[2, 3, 2, 5, 8, 2, 3];
-console.log(maisRepeticoes(teste));
+console.log(somatorio(5));
