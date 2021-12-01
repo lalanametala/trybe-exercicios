5ed75f62817ff94f58c40a1b022dd9e5d6758631
Lalá Nametala <lalanametala@gmail.com>
2021-12-01 18:09:45 -0300

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -5,8 +5,30 @@ let info = {
     recorrente: 'Sim'
 };
 
-  for (let key in info){
-    console.log(info[key]);
-  }
 
-  
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+for (let key in info){
+    if (key=='recorrente'){
+        if (info[key]==info2[key]) {
+            if (info[key]=='Sim'){
+                console.log("Ambos recorrentes");
+            } else {
+                console.log("Nenhum é recorrente");
+            }
+        } else {
+            if (info[key]=='Sim'){
+                console.log("Recorrente e não recorrente");
+            } else {
+                console.log("Não recorrente e recorrente");
+            }
+        }
+    } else {
+        console.log(info[key] + " e "+ info2[key]);
+    }
+}
