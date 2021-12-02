6715122ec45b073d6f8714502b6a7f3639a41d00
Lalá Nametala <lalanametala@gmail.com>
2021-12-02 14:01:51 -0300

    limpando arquivo para fazer o exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -1,15 +1,3 @@
 //Bônus
 
-function vectorUnifier(vetor){
-    let resposta = [];
-    for (let array of vetor){
-        for (let num of array){
-            resposta.push (num);
-        }
-    }
-    return resposta;
-}
-
-
-let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
-console.log(vectorUnifier(vector));
+//COMPLETEI ATE O 2!!!
