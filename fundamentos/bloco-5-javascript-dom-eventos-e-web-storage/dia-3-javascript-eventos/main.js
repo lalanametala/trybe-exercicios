a106aedf1f6b16e8990e17edc35dc330a5b30988
Lalá Nametala <lalanametala@gmail.com>
2021-12-07 18:32:49 -0300

    exemplo finalizado

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/main.js
@@ -12,18 +12,23 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 
-function addClassTech () {
-    firstLi.className = 'tech';
-}
-
-
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+function mudaClasse (event) {
+    const elementoTech = document.querySelector('.tech');
+    elementoTech.classList.remove('tech');
+    event.target.classList.add('tech');
+    input.value = '';
+}
+firstLi.addEventListener('click', mudaClasse);
+secondLi.addEventListener('click', mudaClasse);
+thirdLi.addEventListener('click', mudaClasse);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
-const caixaTextoAlterado = document.querySelector(".tech");
 function alteraTextoCaixa () {
+    const caixaTextoAlterado = document.querySelector(".tech");
     caixaTextoAlterado.innerText = input.value;
 }
 input.addEventListener("keyup", alteraTextoCaixa);
@@ -32,8 +37,22 @@ input.addEventListener("keyup", alteraTextoCaixa);
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
 
+myWebpage.addEventListener('dblclick', function() {
+    window.location.replace('https://lalanametala.github.io/');
+});
+
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+function alteraCorTexto (event) {
+    event.target.style.color = 'green';
+}
+myWebpage.addEventListener('mouseover', alteraCorTexto);
+
+myWebpage.addEventListener('mouseout', function(event) {
+    event.target.style.color = 'unset';
+});
+
 
 // Segue abaixo um exemplo do uso de event.target:
 
