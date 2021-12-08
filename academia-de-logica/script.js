c7f29c5cacf0e5f6befadb9dabff398f39b0b82e
Lalá Nametala <lalanametala@gmail.com>
2021-12-08 13:28:49 -0300

    aaaa

---
+++ academia-de-logica/script.js
@@ -0,0 +1,17 @@
+function atribuirId() {
+    let botao = document.querySelectorAll('.button');
+    for (let index=0; index<botao.length;index+=1){
+        if (index===0){
+            botao[index].id = 'soma';
+            console.log (botao[index]);
+        } else if (index===1){
+            botao[index].id = 'subtracao';
+        } else if (index===2){
+            botao[index].id = 'divisao';
+        } else if (index===3) {
+            botao[index].id = 'multiplicacao';
+        } 
+    }
+}
+atribuirId ();
+
