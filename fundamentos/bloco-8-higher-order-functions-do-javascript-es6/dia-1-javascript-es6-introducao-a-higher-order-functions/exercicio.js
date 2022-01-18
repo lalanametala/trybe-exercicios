d94eb7721f5de7932379c81ce0acc86009c13c4c
Lalá Nametala <lalanametala@gmail.com>
2022-01-18 17:50:11 -0300

    exercicio 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio.js
@@ -1,7 +1,31 @@
-const wakeUp = () => 'Acordando!!';
-const drinkCoffee = () => 'Bora tomar café!!';
-const sleep = () => 'Partiu dormir!!';
+const generateEmail = (anyName) => {
+  const splittedName = anyName.toLowerCase().split(' ');
+  let email = '';
+  for (let index = 0; index < splittedName.length; index +=1) {
+    if (index !== splittedName.length - 1) {
+      email += `${splittedName[index]}_`;
+    } else {
+      email += `${splittedName[index]}@trybe.com`;
+    }
+  }
+  return email;
+}
 
-const doingThings = (func) => console.log(func());
+const employeeInfo = (fullName) => {
+  const newEmployee = {
+    name: fullName,
+    email: generateEmail(fullName),
+  };
+  return newEmployee;
+};
 
-doingThings(sleep);
+const newEmployees = (myFunction) => {
+  const employees = {
+    id1: myFunction('Pedro Guerra'),
+    id2: myFunction('Luiza Drumond'),
+    id3: myFunction('Carla Paiva'), 
+    }
+  return employees;
+};
+
+console.log(newEmployees(employeeInfo));
