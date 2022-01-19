f019ba69b1b9952287ce7943a53cec525e6c18bb
Lalá Nametala <lalanametala@gmail.com>
2022-01-19 14:44:19 -0300

    fixaçao find

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios.js
@@ -1,14 +1,23 @@
-const emailListInData = [
-  'roberta@email.com',
-  'paulo@email.com',
-  'anaroberta@email.com',
-  'fabiano@email.com',
-];
+const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-};
+const findDivisibleBy3And5 = () => numbers.find((number) => number % 3 === 0 && number % 5 === 0);
 
-emailListInData.forEach((item) => {
-  showEmailList(item);
-});
+console.log(findDivisibleBy3And5());
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
+
+console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === '31031685').title;
+}
+
+console.log(findMusic('31031685'))
