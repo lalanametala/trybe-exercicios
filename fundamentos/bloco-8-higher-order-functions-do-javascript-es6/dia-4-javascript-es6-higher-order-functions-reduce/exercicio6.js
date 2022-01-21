6c2a71a5364f748601cf53ce15a624bd2add2d21
Lalá Nametala <lalanametala@gmail.com>
2022-01-21 19:12:52 -0300

    exercicio 6

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
@@ -0,0 +1,19 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  return students.map((student, index) => {
+    return {
+      name: student,
+      average: grades[index].reduce((sum, grade) => sum + grade) / grades[index].length
+    }
+  });
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+console.log(studentAverage());
