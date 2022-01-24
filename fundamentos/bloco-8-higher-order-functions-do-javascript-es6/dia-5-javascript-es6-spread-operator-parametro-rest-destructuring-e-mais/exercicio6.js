ad617678ccb5fa637ac44de8de13cec0c7970fef
Lalá Nametala <lalanametala@gmail.com>
2022-01-24 19:12:04 -0300

    exercicio 6

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio6.js
@@ -0,0 +1,11 @@
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([car, factory, year]) => ({
+  car,
+  factory,
+  year,
+});
+
+console.log(toObject(palio));
