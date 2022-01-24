5d16f6152bc5abf0bcba182e903cf53d44f48e77
Lalá Nametala <lalanametala@gmail.com>
2022-01-24 15:54:50 -0300

    fixaçao default parameters

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
@@ -74,3 +74,7 @@ const getPosition = (latitude, longitude) => ({
   longitude});
 
 console.log(getPosition(-19.8157, -43.9542));
+
+const multiply = (number, value = 1) => number * value;
+
+console.log(multiply(8));
