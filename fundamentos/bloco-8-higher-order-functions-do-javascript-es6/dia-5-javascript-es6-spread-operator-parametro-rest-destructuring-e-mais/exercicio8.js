75575939df9e8981e105f9cc7dcc83ef748fa414
Lalá Nametala <lalanametala@gmail.com>
2022-01-24 19:17:25 -0300

    exercicio 8

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio8.js
@@ -0,0 +1,5 @@
+const greet = (personName, greeting = 'Hi') => `${greeting}, ${personName}`
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
