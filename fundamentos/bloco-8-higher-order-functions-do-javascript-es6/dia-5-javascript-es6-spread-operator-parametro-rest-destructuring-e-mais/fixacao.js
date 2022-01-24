f065f28ccc464a826e9c377e95767e454ce7fc9e
Lalá Nametala <lalanametala@gmail.com>
2022-01-24 15:39:52 -0300

    fixaçao array destructuring

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
@@ -26,3 +26,31 @@ const trappistEnterprise = {...user, ...jobInfos};
 const printInfo = ({ name, age, nationality, profession, squad, squadInitials}) => `Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`;
 
 console.log(printInfo(trappistEnterprise));
+
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+const [message, func] = saudacoes;
+
+func(message);
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida, animal, bebida] = [bebida, comida, animal];
+
+console.log(comida, animal, bebida);
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+[,,,...sohPares] = numerosPares
+
+console.log(numerosPares); // [6, 8, 10, 12];
+console.log(sohPares);
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
