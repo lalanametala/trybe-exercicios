1874b2b6c68066c7220f7038335701a10ee9ab18
Lalá Nametala <lalanametala@gmail.com>
2022-01-24 19:06:16 -0300

    exercicio 4

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio4.js
@@ -0,0 +1,35 @@
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+// escreva filterPeople abaixo
+
+const filterPeople = () => {
+  return people.filter(({ nationality, bornIn }) => nationality === 'Australian' && 1900 < bornIn && bornIn <= 2000);
+}
+
+console.log(filterPeople());
