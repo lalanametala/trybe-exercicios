c73a2ee2fa63ffed49297923257a6cbe969ce344
Lalá Nametala <lalanametala@gmail.com>
2022-01-24 15:46:47 -0300

    fixaçao default destructuring

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/fixacao.js
@@ -53,4 +53,18 @@ let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 console.log(numerosPares); // [6, 8, 10, 12];
 console.log(sohPares);
 
-// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
