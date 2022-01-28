d36419be4fb922cd47f7cfae0189cf548eca0f06
Lalá Nametala <lalanametala@gmail.com>
2022-01-28 18:23:04 -0300

    exercicio 4

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixaçao.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixaçao.js
@@ -1,12 +1,10 @@
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
 };
 
-getPlanet(); // imprime Marte depois de 4 segundos
+const sendMarsTemperature = (delay, callbackTemp) => setTimeout(() => console.log(`Mars temperature is: ${callbackTemp()} degree Celsius`), delay());
+
+sendMarsTemperature(messageDelay, getMarsTemperature); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
