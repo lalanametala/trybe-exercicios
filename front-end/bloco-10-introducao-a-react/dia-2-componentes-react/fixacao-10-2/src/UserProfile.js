bf8e65a7646d1ff19646e6df5916931f7b66c7a5
Lalá Nametala <lalanametala@gmail.com>
2022-02-17 18:20:59 -0300

    fixaçao

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao-10-2/src/UserProfile.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixacao-10-2/src/UserProfile.js
@@ -3,11 +3,24 @@ import Image from './Image';
 
 class UserProfile extends React.Component {
   render() {
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      }
+    ];
+
     return (
-      <div>
-        <p> {this.props.user.name} </p>
-        <p> {this.props.user.email} </p>
-        <Image source={this.props.user.avatar} alternativeText="User avatar" />
+      <div className="App">
+        {users.map((userElement, index) => <UserProfile user={userElement} key={index} />)}        
       </div>
     );
   }
