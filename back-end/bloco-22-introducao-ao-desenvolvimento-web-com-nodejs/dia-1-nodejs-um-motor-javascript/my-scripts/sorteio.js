29f77bf29d6efc610360bf7ed70818553e052d10
Lalá Nametala <lalanametala@gmail.com>
2022-06-06 17:56:36 -0300

    exercicio 7

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/sorteio.js
@@ -0,0 +1,8 @@
+const { questionInt } = require('readline-sync');
+
+const guessedNumber = questionInt('Guess the number: ');
+
+const randomNumber = Math.floor(Math.random()*11);
+
+if (guessedNumber === randomNumber) console.log('Parabéns, número correto!');
+if (guessedNumber !== randomNumber) console.log(`Opa, não foi dessa vez. O número era ${randomNumber}`);
