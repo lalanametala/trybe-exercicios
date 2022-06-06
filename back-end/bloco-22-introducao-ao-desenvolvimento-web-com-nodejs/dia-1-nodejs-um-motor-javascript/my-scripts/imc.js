52d56b40eb51c92c82120fb541efc08ec848791b
Lalá Nametala <lalanametala@gmail.com>
2022-06-06 15:05:49 -0300

    exercicios 3 e 4

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -1,1 +1,6 @@
-console.log(`IMC = ${75/(1.74*1.74)}`);
+const readline = require('readline-sync');
+
+const weight = readline.questionFloat('Qual seu peso? ');
+const height = readline.questionFloat('Qual sua altura? ');
+
+console.log(`IMC = ${weight/(height*height)}`);
