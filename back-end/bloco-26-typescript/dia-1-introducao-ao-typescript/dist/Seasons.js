8b813ec502c6fd664cd8090a5e188827f572265e
Lalá Nametala <lalanametala@gmail.com>
2022-07-26 18:56:17 -0300

    exercícios 2, 3 e 4

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/dist/Seasons.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Seasons;
+(function (Seasons) {
+    Seasons["OUTONO"] = "Outono";
+    Seasons["INVERNO"] = "Inverno";
+    Seasons["PRIMAVERA"] = "Primavera";
+    Seasons["VERAO"] = "Ver\u00E3o";
+})(Seasons || (Seasons = {}));
+exports.default = Seasons;
